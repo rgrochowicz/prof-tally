a73a922dcc521b9e2a59db2334ca07c4ba4c8214
Rob Grochowicz <robert.grochowicz@gmail.com>
2014-10-21 19:13:53 -0400

    Update database schema to support course enrollment history

--- scraper/db.sql
+++ scraper/db.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.3.5
 -- Dumped by pg_dump version 9.3.1
--- Started on 2014-10-11 20:35:20
+-- Started on 2014-10-21 19:11:07
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -14,26 +14,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 1956 (class 1262 OID 16386)
--- Name: tally; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE tally WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
-
-
-ALTER DATABASE tally OWNER TO postgres;
-
-\connect tally
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
--- TOC entry 174 (class 3079 OID 11756)
+-- TOC entry 176 (class 3079 OID 11756)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -41,8 +22,8 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 1959 (class 0 OID 0)
--- Dependencies: 174
+-- TOC entry 1968 (class 0 OID 0)
+-- Dependencies: 176
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -52,7 +33,7 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 SET search_path = public, pg_catalog;
 
 --
--- TOC entry 520 (class 1247 OID 16396)
+-- TOC entry 522 (class 1247 OID 16396)
 -- Name: weekday; Type: TYPE; Schema: public; Owner: tally
 --
 
@@ -71,6 +52,46 @@ ALTER TYPE public.weekday OWNER TO tally;
 SET default_tablespace = '';
 
 SET default_with_oids = false;
+
+--
+-- TOC entry 175 (class 1259 OID 16473)
+-- Name: course_enrollments; Type: TABLE; Schema: public; Owner: tally; Tablespace: 
+--
+
+CREATE TABLE course_enrollments (
+    id integer NOT NULL,
+    crn integer,
+    max integer,
+    enrolled integer,
+    "time" timestamp with time zone
+);
+
+
+ALTER TABLE public.course_enrollments OWNER TO tally;
+
+--
+-- TOC entry 174 (class 1259 OID 16471)
+-- Name: course_enrollments_id_seq; Type: SEQUENCE; Schema: public; Owner: tally
+--
+
+CREATE SEQUENCE course_enrollments_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.course_enrollments_id_seq OWNER TO tally;
+
+--
+-- TOC entry 1969 (class 0 OID 0)
+-- Dependencies: 174
+-- Name: course_enrollments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tally
+--
+
+ALTER SEQUENCE course_enrollments_id_seq OWNED BY course_enrollments.id;
+
 
 --
 -- TOC entry 173 (class 1259 OID 16443)
@@ -109,7 +130,7 @@ CREATE SEQUENCE course_times_id_seq
 ALTER TABLE public.course_times_id_seq OWNER TO tally;
 
 --
--- TOC entry 1960 (class 0 OID 0)
+-- TOC entry 1970 (class 0 OID 0)
 -- Dependencies: 172
 -- Name: course_times_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tally
 --
@@ -158,7 +179,7 @@ CREATE SEQUENCE courses_id_seq
 ALTER TABLE public.courses_id_seq OWNER TO tally;
 
 --
--- TOC entry 1961 (class 0 OID 0)
+-- TOC entry 1971 (class 0 OID 0)
 -- Dependencies: 170
 -- Name: courses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tally
 --
@@ -167,7 +188,15 @@ ALTER SEQUENCE courses_id_seq OWNED BY courses.id;
 
 
 --
--- TOC entry 1839 (class 2604 OID 16446)
+-- TOC entry 1846 (class 2604 OID 16476)
+-- Name: id; Type: DEFAULT; Schema: public; Owner: tally
+--
+
+ALTER TABLE ONLY course_enrollments ALTER COLUMN id SET DEFAULT nextval('course_enrollments_id_seq'::regclass);
+
+
+--
+-- TOC entry 1845 (class 2604 OID 16446)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: tally
 --
 
@@ -175,7 +204,7 @@ ALTER TABLE ONLY course_times ALTER COLUMN id SET DEFAULT nextval('course_times_
 
 
 --
--- TOC entry 1838 (class 2604 OID 16435)
+-- TOC entry 1844 (class 2604 OID 16435)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: tally
 --
 
@@ -183,7 +212,16 @@ ALTER TABLE ONLY courses ALTER COLUMN id SET DEFAULT nextval('courses_id_seq'::r
 
 
 --
--- TOC entry 1843 (class 2606 OID 16451)
+-- TOC entry 1852 (class 2606 OID 16478)
+-- Name: course_enrollments_pkey; Type: CONSTRAINT; Schema: public; Owner: tally; Tablespace: 
+--
+
+ALTER TABLE ONLY course_enrollments
+    ADD CONSTRAINT course_enrollments_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 1850 (class 2606 OID 16451)
 -- Name: course_times_pkey; Type: CONSTRAINT; Schema: public; Owner: tally; Tablespace: 
 --
 
@@ -192,7 +230,7 @@ ALTER TABLE ONLY course_times
 
 
 --
--- TOC entry 1841 (class 2606 OID 16440)
+-- TOC entry 1848 (class 2606 OID 16440)
 -- Name: courses_pkey; Type: CONSTRAINT; Schema: public; Owner: tally; Tablespace: 
 --
 
@@ -201,7 +239,7 @@ ALTER TABLE ONLY courses
 
 
 --
--- TOC entry 1844 (class 2606 OID 16452)
+-- TOC entry 1853 (class 2606 OID 16452)
 -- Name: course_times_course_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: tally
 --
 
@@ -210,7 +248,7 @@ ALTER TABLE ONLY course_times
 
 
 --
--- TOC entry 1958 (class 0 OID 0)
+-- TOC entry 1967 (class 0 OID 0)
 -- Dependencies: 5
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -221,7 +259,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2014-10-11 20:35:21
+-- Completed on 2014-10-21 19:11:08
 
 --
 -- PostgreSQL database dump complete
