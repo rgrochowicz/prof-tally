2e829ff123628c7bec2c8ae6c84ced1dcc6b6c8d
Rob Grochowicz <robert.grochowicz@gmail.com>
2014-11-05 17:38:48 -0500

    Change the scheduler to use redis for the intense calculations instead of postgres

--- scheduler/main.go
+++ scheduler/main.go
@@ -1,155 +1,126 @@
 package main
 
 import (
+	_ "database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-    _ "database/sql"
-    "github.com/jmoiron/sqlx"
-    "log"
-    "strings"
-    "fmt"
-    "encoding/json"
-    "time"
+	"log"
+	"time"
 )
 
 var (
-	db *sqlx.DB
+	db     *sqlx.DB
+	c      redis.Conn
+	crnMap map[string]Course
 )
 
 type CourseEntry struct {
-	Title string
-	Crn string
+	Title   string
+	Crn     string
 	Weekday string
-	Start string
-	Length string
+	Start   string
+	Length  string
 }
 
 type Course struct {
-	Crn string
+	Crn   string
 	Title string
 	Times []CourseTime
 }
 
 type CourseTime struct {
 	Weekday string
-	Start string
-	Length string
+	Start   string
+	Length  string
 }
 
-type Schedule struct {
+type CrnSchedule struct {
+	Crns []string
+}
+
+type TrueSchedule struct {
 	Courses []Course
 }
 
-
-//Takes a slice of courses and returns it formatted for PostgreSQL use
-//Used for the ANY expression
-//ex. [1 2 3] -> {1,2,3}
-func coursesToCrnList(courses []Course) string {
-	crnArray := []string{}
-	for _, course := range courses {
-		crnArray = append(crnArray, course.Crn)
-	}
-	return fmt.Sprintf("{%s}", strings.Join(crnArray, ","))
-}
-
-//Converts from a slice of courses to a map that maps from crn to
-// the relevant entries
-func groupByCrn(courses []CourseEntry) map[string][]CourseEntry {
-	result := map[string][]CourseEntry{}
-	for _, course := range courses {
-		if _, ok := result[course.Crn]; ok {
-			result[course.Crn] = append(result[course.Crn], course)
-		} else {
-			result[course.Crn] = []CourseEntry{
-				course,
-			}
-		}
-	}
-
-	return result
-}
-
-//Takes a query and params and returns the results in a crn to course list map
-func queryCourse(query string, params ...interface{}) map[string]Course {
-	result := map[string]Course{}
-
-	entries := []CourseEntry{}
-	db.Select(&entries, query, params...)
-	mapped := groupByCrn(entries)
-
-	for crn, courseEntries := range mapped {
-
-		//format the result based on the api spec
-		times := []CourseTime{}
-		for _, entry := range courseEntries {
-			times = append(times, CourseTime{
-				Weekday: entry.Weekday,
-				Start: entry.Start,
-				Length: entry.Length,
-			})
-		}
-
-		result[crn] = Course{
-			Crn: crn,
-			Title: courseEntries[0].Title,
-			Times: times,
-		}
-	}
-
-	return result
-
-}
-
-func getNext(courses []Course, titles []string) []Schedule {
-
-	//when there are no titles left to search, return the list of courses
-	if len(titles) == 0 {
-		return []Schedule{
-			Schedule{
-				Courses: courses,
-			},
-		}
-	}
-
-	nextTitle := titles[0]
-
-	//get the courses with a certain title that doesn't overlap an array of crns
-	entries := queryCourse(`with taken_times as (
-	select weekday, start_time, length
-	from course_times
-	where course_crn = ANY($1)
-), relevant_crns as (
-	select crn
-	from courses
-	where title = $2
-), excluded_crns as (
-	select course_crn
-	from course_times
-	where course_crn in (select crn from relevant_crns) and
-	exists (
-		select 1
-		from taken_times
-		where 
-			(taken_times.start_time, taken_times.length) overlaps (course_times.start_time, course_times.length) and
-			weekday = course_times.weekday
-	)
-)
-select
+//Populate the translation map from crn to course
+func populateTranslations() {
+	courses := []CourseEntry{}
+	db.Select(&courses, `select
 	courses.title,
 	courses.crn,
 	course_times.weekday,
 	to_char(course_times.start_time, 'HH24:MI') as start,
 	to_char(course_times.length, 'HH24:MI') as length
 from
-	courses,
-	course_times
+	course_times,
+	courses
 where
-	course_times.course_crn = courses.crn and
-	crn in (select crn from relevant_crns) and
-	crn not in (select course_crn from excluded_crns)`, coursesToCrnList(courses), nextTitle)
+	course_times.course_crn = courses.crn`)
 
-	finalResult := []Schedule{}
+	crnMap = map[string]Course{}
+	for _, course := range courses {
+		if _, ok := crnMap[course.Crn]; ok {
+			existing := crnMap[course.Crn]
+			existing.Times = append(crnMap[course.Crn].Times, CourseTime{
+				Weekday: course.Weekday,
+				Start:   course.Start,
+				Length:  course.Length,
+			})
+		} else {
+			crnMap[course.Crn] = Course{
+				Title: course.Title,
+				Crn:   course.Crn,
+				Times: []CourseTime{},
+			}
+		}
+	}
+}
+
+//Translate an array of array of crns to schedules
+func translateSchedules(schedules []CrnSchedule) []TrueSchedule {
+	result := []TrueSchedule{}
+	for _, schedule := range schedules {
+		newSchedule := TrueSchedule{}
+		for _, crn := range schedule.Crns {
+			newSchedule.Courses = append(newSchedule.Courses, crnMap[crn])
+		}
+		result = append(result, newSchedule)
+	}
+
+	return result
+}
+
+func getNext(crns []string, titles []string) []CrnSchedule {
+
+	//when there are not titles left, return the crns collected
+	if len(titles) == 0 {
+		return []CrnSchedule{
+			CrnSchedule{
+				Crns: crns,
+			},
+		}
+	}
+
+	nextTitle := titles[0]
+
+	crnArgs := []string{}
+	for _, crn := range crns {
+		crnArgs = append(crnArgs, fmt.Sprintf("crn:%s", crn))
+	}
+
+	//get all available courses based on set intersections
+	entries, err := redis.Strings(c.Do("SINTER", redis.Args{}.AddFlat(crnArgs).Add(fmt.Sprintf("title:%s", nextTitle))...))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+
+	finalResult := []CrnSchedule{}
 	for _, entry := range entries {
-		results := getNext(append(courses, entry), titles[1:])
+		results := getNext(append(crns, entry), titles[1:])
 		for _, result := range results {
 			finalResult = append(finalResult, result)
 		}
@@ -163,27 +134,36 @@ func main() {
 
 	var err error
 
-	//connect to the postgres db
+	//connect to postgres
 	db, err = sqlx.Connect("postgres", "user=tally dbname=tally password=tally sslmode=disable")
-    if err != nil {
-        log.Fatalln(err)
-    }
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
 
-    defer db.Close()
-    titles := []string{"FOUNDATIONS COMP SCI","OBJ-ORIENT PRGRM/DATA ABSTR","LINEAR ALGEBRA","COLLEGE COMPOSITION II"}
-    
-    startTime := time.Now()
+	//connect to redis
+	c, err = redis.Dial("tcp", ":6379")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer c.Close()
 
-    //get all possible schedules
-    schedules := getNext([]Course{}, titles)
+	titles := []string{"FOUNDATIONS COMP SCI", "OBJ-ORIENT PRGRM/DATA ABSTR", "LINEAR ALGEBRA", "COLLEGE COMPOSITION II", "PUBLIC SPEAKING"}
 
-    log.Println(time.Since(startTime))
+	startTime := time.Now()
 
-    jsonBytes, err := json.Marshal(schedules)
-    if err != nil {
-    	log.Fatalln(err)
-    }
-    log.Println(len(jsonBytes))
-    log.Println(len(schedules))
+	populateTranslations()
+	crnSchedules := getNext([]string{}, titles)
+	trueSchedules := translateSchedules(crnSchedules)
+
+	jsonBytes, err := json.Marshal(trueSchedules)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(time.Since(startTime))
+
+	log.Println(len(crnSchedules))
+	log.Println(len(jsonBytes))
 
 }
