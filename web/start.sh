f3274b76c7dcf3263fdf91b13da66d7f511da2bb
Rob Grochowicz <robert.grochowicz@gmail.com>
2014-11-10 16:57:45 -0500

    Fix nginx conf and gzip proxied requests

--- web/start.sh
+++ web/start.sh
@@ -5,13 +5,14 @@ server {
 	listen 80;
 	server_name _;
 
-	location / {
-		root /app;
-		index index.html;
-	}
+	root /app;
 
 	location /api {
-		proxy_pass http://$API_HOST:$API_PORT/api/;
+
+		gzip             on;
+		gzip_proxied     any;
+		gzip_types       text/css text/plain text/xml application/xml application/javascript application/x-javascript text/javascript application/json text/x-json;
+		proxy_pass http://$API_HOST:$API_PORT;
 	}
 
 }
