c04a866f9c614ccfd24a7aa7d035aad4941f2429
Rob Grochowicz <robert.grochowicz@gmail.com>
2014-11-10 02:21:31 -0500

    Add course attributes

--- postgres/db.sql
+++ postgres/db.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.3.5
 -- Dumped by pg_dump version 9.3.1
--- Started on 2014-10-26 19:50:26
+-- Started on 2014-11-10 00:05:51
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -14,7 +14,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 175 (class 3079 OID 11756)
+-- TOC entry 177 (class 3079 OID 11756)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -22,8 +22,8 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 1965 (class 0 OID 0)
--- Dependencies: 175
+-- TOC entry 1981 (class 0 OID 0)
+-- Dependencies: 177
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -33,7 +33,7 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 SET search_path = public, pg_catalog;
 
 --
--- TOC entry 521 (class 1247 OID 16396)
+-- TOC entry 523 (class 1247 OID 16396)
 -- Name: weekday; Type: TYPE; Schema: public; Owner: tally
 --
 
@@ -52,6 +52,19 @@ ALTER TYPE public.weekday OWNER TO tally;
 SET default_tablespace = '';
 
 SET default_with_oids = false;
+
+--
+-- TOC entry 175 (class 1259 OID 24637)
+-- Name: course_attrs; Type: TABLE; Schema: public; Owner: tally; Tablespace: 
+--
+
+CREATE TABLE course_attrs (
+    short text NOT NULL,
+    name text
+);
+
+
+ALTER TABLE public.course_attrs OWNER TO tally;
 
 --
 -- TOC entry 171 (class 1259 OID 16473)
@@ -85,7 +98,7 @@ CREATE SEQUENCE course_enrollments_id_seq
 ALTER TABLE public.course_enrollments_id_seq OWNER TO tally;
 
 --
--- TOC entry 1966 (class 0 OID 0)
+-- TOC entry 1982 (class 0 OID 0)
 -- Dependencies: 170
 -- Name: course_enrollments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tally
 --
@@ -130,7 +143,7 @@ CREATE SEQUENCE course_times_id_seq
 ALTER TABLE public.course_times_id_seq OWNER TO tally;
 
 --
--- TOC entry 1967 (class 0 OID 0)
+-- TOC entry 1983 (class 0 OID 0)
 -- Dependencies: 173
 -- Name: course_times_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tally
 --
@@ -163,7 +176,20 @@ CREATE TABLE courses (
 ALTER TABLE public.courses OWNER TO tally;
 
 --
--- TOC entry 1842 (class 2604 OID 16476)
+-- TOC entry 176 (class 1259 OID 24645)
+-- Name: courses_and_attrs; Type: TABLE; Schema: public; Owner: tally; Tablespace: 
+--
+
+CREATE TABLE courses_and_attrs (
+    crn integer,
+    attr text
+);
+
+
+ALTER TABLE public.courses_and_attrs OWNER TO tally;
+
+--
+-- TOC entry 1852 (class 2604 OID 16476)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: tally
 --
 
@@ -171,7 +197,7 @@ ALTER TABLE ONLY course_enrollments ALTER COLUMN id SET DEFAULT nextval('course_
 
 
 --
--- TOC entry 1843 (class 2604 OID 24612)
+-- TOC entry 1853 (class 2604 OID 24612)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: tally
 --
 
@@ -179,7 +205,16 @@ ALTER TABLE ONLY course_times ALTER COLUMN id SET DEFAULT nextval('course_times_
 
 
 --
--- TOC entry 1845 (class 2606 OID 16478)
+-- TOC entry 1863 (class 2606 OID 24644)
+-- Name: course_attrs_pkey; Type: CONSTRAINT; Schema: public; Owner: tally; Tablespace: 
+--
+
+ALTER TABLE ONLY course_attrs
+    ADD CONSTRAINT course_attrs_pkey PRIMARY KEY (short);
+
+
+--
+-- TOC entry 1855 (class 2606 OID 16478)
 -- Name: course_enrollments_pkey; Type: CONSTRAINT; Schema: public; Owner: tally; Tablespace: 
 --
 
@@ -188,7 +223,7 @@ ALTER TABLE ONLY course_enrollments
 
 
 --
--- TOC entry 1849 (class 2606 OID 24617)
+-- TOC entry 1861 (class 2606 OID 24617)
 -- Name: course_times_pkey; Type: CONSTRAINT; Schema: public; Owner: tally; Tablespace: 
 --
 
@@ -197,7 +232,7 @@ ALTER TABLE ONLY course_times
 
 
 --
--- TOC entry 1847 (class 2606 OID 24606)
+-- TOC entry 1857 (class 2606 OID 24606)
 -- Name: courses_pkey; Type: CONSTRAINT; Schema: public; Owner: tally; Tablespace: 
 --
 
@@ -206,7 +241,23 @@ ALTER TABLE ONLY courses
 
 
 --
--- TOC entry 1850 (class 2606 OID 24618)
+-- TOC entry 1859 (class 1259 OID 24636)
+-- Name: course_crn_idx; Type: INDEX; Schema: public; Owner: tally; Tablespace: 
+--
+
+CREATE INDEX course_crn_idx ON course_times USING btree (course_crn);
+
+
+--
+-- TOC entry 1858 (class 1259 OID 24635)
+-- Name: title_idx; Type: INDEX; Schema: public; Owner: tally; Tablespace: 
+--
+
+CREATE INDEX title_idx ON courses USING btree (title);
+
+
+--
+-- TOC entry 1864 (class 2606 OID 24618)
 -- Name: course_times_course_crn_fkey; Type: FK CONSTRAINT; Schema: public; Owner: tally
 --
 
@@ -215,7 +266,25 @@ ALTER TABLE ONLY course_times
 
 
 --
--- TOC entry 1964 (class 0 OID 0)
+-- TOC entry 1866 (class 2606 OID 24656)
+-- Name: courses_and_attrs_attr_fkey; Type: FK CONSTRAINT; Schema: public; Owner: tally
+--
+
+ALTER TABLE ONLY courses_and_attrs
+    ADD CONSTRAINT courses_and_attrs_attr_fkey FOREIGN KEY (attr) REFERENCES course_attrs(short);
+
+
+--
+-- TOC entry 1865 (class 2606 OID 24651)
+-- Name: courses_and_attrs_crn_fkey; Type: FK CONSTRAINT; Schema: public; Owner: tally
+--
+
+ALTER TABLE ONLY courses_and_attrs
+    ADD CONSTRAINT courses_and_attrs_crn_fkey FOREIGN KEY (crn) REFERENCES courses(crn);
+
+
+--
+-- TOC entry 1980 (class 0 OID 0)
 -- Dependencies: 5
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -226,7 +295,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2014-10-26 19:50:27
+-- Completed on 2014-11-10 00:05:52
 
 --
 -- PostgreSQL database dump complete
--- scraper/scrape.py
+++ scraper/scrape.py
@@ -17,7 +17,8 @@ class Course(object):
 		self.subject = str(children[1].string)
 		self.course_num = str(children[2].string)
 		self.section = str(children[3].string)
-		# 4 is "part of term", 5 is "session" (time of day)
+		# 4 is "part of term"
+		# 5 is "session" (time of day)
 		self.title = str(list(children[6].children)[0].string)
 		self.professors = list(map(str, children[7].stripped_strings))
 		self.raw_times = list(map(str, children[8].stripped_strings))
@@ -52,6 +53,121 @@ class CourseTime(object):
 			self.type = match.group('type')
 			self.invalid = False
 
+class Attribute(object):
+
+	def __init__(self, node):
+		self.short = node['value']
+
+		#the names come out as "short - name", so the names are extracted using regex
+		match = re.match(r'.*?\s-\s(.*)', str(node.string))
+		if match:
+			self.name = match.group(1)
+
+	def __str__(self):
+		return "{} - {}".format(self.short, self.name)
+
+def get_attributes():
+
+	#connect to postgres
+	conn = psycopg2.connect(database=os.environ['POSTGRES_DATABASE'],
+		user=os.environ['POSTGRES_USER'],
+		password=os.environ['POSTGRES_PASSWORD'],
+		host=os.environ['POSTGRES_HOST'],
+		port=os.environ['POSTGRES_PORT'])
+	cur = conn.cursor()
+
+	payload = {
+		"Select Term": "Select Term",
+		"term": "201520",
+		"task": "Section_Tally"
+	}
+	r = requests.post("http://banner.rowan.edu/reports/reports.pl?task=Section_Tally", data=payload)
+	soup = bs(r.text)
+
+	#this uses the attribute dropdown to get its values
+	attribute_select = soup.select("select[name=attr]")[0]
+	course_attributes = []
+
+	for option in attribute_select.select("option"):
+		attr = Attribute(option)
+
+		#ignore the ALL attribute
+		if attr.short != 'ALL':
+			course_attributes.append(attr)
+
+	for course_attribute in course_attributes:
+		cur.execute(
+			"""INSERT INTO course_attrs (
+					short,
+					name
+				) VALUES (%s, %s)
+			""", (
+				course_attribute.short,
+				course_attribute.name
+			))
+	conn.commit()
+
+
+	#loop through and do a separate request for each attribute
+	for course_attribute in course_attributes:
+		attr_search = {
+			"term": "201520",
+			"task": "Section_Tally",
+			"coll": "ALL",
+			"dept": "ALL",
+			"subj": "ALL",
+			"ptrm": "ALL",
+			"sess": "ALL",
+			"prof": "ALL",
+			"attr": course_attribute.short,
+			"camp": "ALL",
+			"bldg": "ALL",
+			"Search": "Search"
+		}
+
+		for course in get_courses_in_page(requests.post("http://banner.rowan.edu/reports/reports.pl?task=Section_Tally", data=attr_search).text):
+
+			cur.execute(
+				"""INSERT INTO courses_and_attrs (
+						crn,
+						attr
+					) VALUES (%s, %s)
+				""", (
+					course.crn,
+					course_attribute.short
+				))
+
+	#commit and close connection
+	conn.commit()
+	cur.close()
+	conn.close()
+
+#given page html, return the courses
+def get_courses_in_page(text):
+
+	text = text.replace('></div><div>', '>') #fix html error
+
+	soup = bs(text) 
+	class_table = soup.find("table", class_="report")
+
+	courses = []
+
+	#loop through all table rows
+	for tr in class_table.tbody.children:
+
+		#skip strings and a tags
+		if isinstance(tr, NavigableString): continue
+		if tr.name == "a": continue
+
+		#make sure it's the correct type of row
+		if 'style' in tr.attrs and tr.attrs['style'] == 'background-color:inherit':
+
+			#also make sure it's not cancelled
+			if 'class' not in tr.attrs or tr.attrs['class'] != 'cancel':
+				course = Course(tr)
+				courses.append(course)
+
+	return courses
 
 def main():
 
@@ -71,7 +187,6 @@ def main():
 		"Search": "Search"
 	}
 
-
 	# download and cache in page.html
 	text = ""
 	if os.path.isfile('page.html'):
@@ -83,49 +198,37 @@ def main():
 		with open('page.html', 'w') as page:
 			page.write(text)
 
-	courses = []
 
-	soup = bs(text)
-	class_table = soup.find("table", class_="report")
-
-	#loop through all table rows
-	for tr in class_table.tbody.children:
-		#skip strings and a tags
-		if isinstance(tr, NavigableString): continue
-		if tr.name == "a": continue
-
-		#make sure it's the correct type of row
-		if 'style' in tr.attrs and tr.attrs['style'] == 'background-color:inherit':
-			courses.append(Course(tr))
+	courses = get_courses_in_page(text)
 
 	conn = psycopg2.connect(database=os.environ['POSTGRES_DATABASE'],
 		user=os.environ['POSTGRES_USER'],
 		password=os.environ['POSTGRES_PASSWORD'],
 		host=os.environ['POSTGRES_HOST'],
 		port=os.environ['POSTGRES_PORT'])
-	
+
 	cur = conn.cursor()
 	for course in courses:
 
 		#put the course into the db
 		cur.execute(
-			"INSERT INTO courses (				\
-				crn,							\
-				subject,						\
-				course_num,						\
-				section,						\
-				title,							\
-				professors,						\
-				campus,							\
-				hours,							\
-				max,							\
-				max_reserved,					\
-				left_reserved,					\
-				enrolled,						\
-				available						\
-			)									\
-			VALUES (%s, %s, %s, %s, %s, %s,		\
-					%s, %s, %s, %s, %s, %s, %s)",
+			"""INSERT INTO courses (
+				crn,
+				subject,
+				course_num,
+				section,
+				title,
+				professors,
+				campus,
+				hours,
+				max,
+				max_reserved,
+				left_reserved,
+				enrolled,
+				available
+			)
+			VALUES (%s, %s, %s, %s, %s, %s,
+					%s, %s, %s, %s, %s, %s, %s)""",
 			(	course.crn,
 				course.subject,
 				course.course_num,
@@ -145,43 +248,46 @@ def main():
 			if time.invalid:
 				#insert a row to fix later
 				cur.execute(
-					"INSERT INTO course_times (						\
-						course_crn,									\
-						weekday,									\
-						start_time,									\
-						length,										\
-						building,									\
-						room,										\
-						type,										\
-						invalid,									\
-						raw_time									\
-					)												\
-					VALUES (%(course_crn)s, null, null, null, null,	\
-							null, null, %(invalid)s, %(raw_time)s)",
-					{	"course_crn": course.crn,
+					"""INSERT INTO course_times (
+						course_crn,
+						weekday,
+						start_time,
+						length,
+						building,
+						room,
+						type,
+						invalid,
+						raw_time
+					)
+					VALUES (%(course_crn)s, null, null, null, null,
+							null, null, %(invalid)s, %(raw_time)s)""",
+					{
+						"course_crn": course.crn,
 						"invalid": time.invalid,
-						"raw_time": time.raw_time	}
+						"raw_time": time.raw_time
+					}
 				)
 			else:
 				#insert a row per day
 				for day in time.days:
 					cur.execute(
-						"INSERT INTO course_times (		\
-							course_crn,					\
-							weekday,					\
-							start_time,					\
-							length,						\
-							building,					\
-							room,						\
-							type,						\
-							invalid,					\
-							raw_time					\
-						)								\
-						VALUES (%(course_crn)s, %(day)s, %(start_time)s,			\
-								%(end_time)s::time - %(start_time)s::time,		\
-								%(building)s, %(room)s,  %(type)s,				\
-								%(invalid)s, %(raw_time)s)",
-						{	"course_crn": course.crn,
+						"""INSERT INTO course_times (
+							course_crn,
+							weekday,
+							start_time,
+							length,
+							building,
+							room,
+							type,
+							invalid,
+							raw_time
+						)
+						VALUES (%(course_crn)s, %(day)s, %(start_time)s,
+								%(end_time)s::time - %(start_time)s::time,
+								%(building)s, %(room)s,  %(type)s,
+								%(invalid)s, %(raw_time)s)""",
+						{
+							"course_crn": course.crn,
 							"day": day,
 							"start_time": time.start_time,
 							"end_time": time.end_time,
@@ -189,13 +295,17 @@ def main():
 							"room": time.room,
 							"type": time.type,
 							"invalid": time.invalid,
-							"raw_time": time.raw_time	}
+							"raw_time": time.raw_time
+						}
 					)
 
 	#commit and close connection
 	conn.commit()
 	cur.close()
 	conn.close()
+
+	#get and associate attributes with courses
+	get_attributes()
 
 if __name__ == '__main__':
 	main()
